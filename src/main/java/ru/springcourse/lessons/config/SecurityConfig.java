bb3bbee72df2ae858f4eb767a605e1a04d218be8
Sam-Ayn <samueleayn@gmail.com>
2020-11-30 21:11:29 +0300

    Initial commit

---
+++ src/main/java/ru/springcourse/lessons/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package ru.springcourse.lessons.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        //Deprecated. just for educational purposes
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        auth.inMemoryAuthentication().withUser(users.username("john").password("test123").roles("EMPLOYEE"));
+        auth.inMemoryAuthentication().withUser(users.username("mary").password("test123").roles("MANAGER"));
+        auth.inMemoryAuthentication().withUser(users.username("susan").password("test123").roles("ADMIN"));
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>Company home page</title>
+</head>
+<body>
+<h2>Company home page</h2>
+<hr>
+Welcome to company home page!
+</body>
+</html>
